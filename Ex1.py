ca2c301ac4b9e8ada4a5f63595209ba43add4687
Saranya Athipatla <saranyaathipatla@Saranyas-Air.hsd1.ca.comcast.net>
2023-10-05 15:10:20 -0700

    my first python program

---
+++ Ex1.py
@@ -0,0 +1,7 @@
+'''Write a program to display the below message (using single and double quotes)
+   Output:
+    This\\t is my<TAB> 'first' program\ in "PYTHON"<NEWLINE>'''
+
+
+print("This\\\\t is my\t 'first' program\\ in \"PYTHON\"\nusing\"")
+print('This\\\\t is my\t \'first\' program\\ in "PYTHON"\nusing\'')
